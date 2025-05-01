21a04ccdb569d349e91ead7b0ac86be41cc074ae
GxAditya <kradi2645@gmail.com>
2025-05-01 22:57:59 +0530

    Day-1

---
+++ IRCTC/app/src/main/java/org/example/entities/User.java
@@ -0,0 +1,15 @@
+package org.example.entities;
+
+import java.util.List;
+
+public class User {
+
+    private String Name ;
+    private String Password ;
+    private String HashPassword;
+    private String userid ;
+
+    private List<Ticket> bookedtickets ;
+
+
+}
