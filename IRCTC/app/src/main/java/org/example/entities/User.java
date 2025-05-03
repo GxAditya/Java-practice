bd868f025d9988d4eeeee0e360c68aa64ce712f9
GxAditya <kradi2645@gmail.com>
2025-05-03 23:38:02 +0530

    Day-3

--- IRCTC/app/src/main/java/org/example/entities/User.java
+++ IRCTC/app/src/main/java/org/example/entities/User.java
@@ -7,9 +7,60 @@ public class User {
     private String Name ;
     private String Password ;
     private String HashPassword;
-    private String userid ;
+    private String userId ;
 
     private List<Ticket> bookedtickets ;
 
 
+    public User(String name, String password, String hashedPassword, List<Ticket> ticketsBooked, String userId){
+        this.Name = name;
+        this.Password = password;
+        this.HashPassword = hashedPassword;
+        this.bookedtickets = ticketsBooked;
+        this.userId = userId;
+    }
+    public User(){}
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getPassword(){
+        return Password;
+    }
+
+    public String getHashedPassword() {
+        return HashPassword;
+    }
+
+    public List<Ticket> getTicketsBooked() {
+        return bookedtickets;
+    }
+
+    public void printTickets(){
+        for (int i = 0; i<bookedtickets.size(); i++){
+            System.out.println(bookedtickets.get(i).getTicketInfo());
+        }
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setName(String name) {
+        this.Name = name;
+    }
+
+    public void setHashedPassword(String hashedPassword) {
+        this.HashPassword = hashedPassword;
+    }
+
+    public void setTicketsBooked(List<Ticket> ticketsBooked) {
+        this.bookedtickets = ticketsBooked;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 }
+
